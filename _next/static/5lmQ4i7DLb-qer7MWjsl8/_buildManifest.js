07b120cdcf3916c537bc37ed68a146a90838f546
Jace Nibarger <jnibarger01@gmail.com>
2026-09-17 21:41:49 -0500

    Deploy ShiftForge 2026-09-17

---
+++ _next/static/5lmQ4i7DLb-qer7MWjsl8/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/shiftforge//_next/:path+",
+        "destination": "/shiftforge/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
